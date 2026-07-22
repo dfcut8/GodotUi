eb51808db2969116f44b56c885edf4ba272e3be6
dfcut8 <dfcut8@proton.me>
2026-07-22 17:41:33 -0400

    focus

---
+++ FocusSamples.cs
@@ -0,0 +1,23 @@
+using Godot;
+
+public partial class FocusSamples : Control
+{
+    private Button? focusedButton;
+    private Label? label;
+    public override void _Ready()
+    {
+        focusedButton = GetNode<Button>("%Button4");
+        label = GetNode<Label>("%Label");
+        focusedButton?.GrabFocus();
+        focusedButton.FocusEntered += OnFocusEntered;
+    }
+
+    private void OnFocusEntered()
+    {
+        GD.Print("Focus Entered!!!");
+        focusedButton?.GrabFocus();
+        label?.Text = focusedButton?.HasFocus().ToString();
+    }
+
+    public override void _Process(double delta) { }
+}
