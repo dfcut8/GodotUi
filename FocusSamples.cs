a232f28541d9dd1f3508a6ca2a8e9c2590910bda
dfcut8 <dfcut8@proton.me>
2026-07-23 12:16:40 -0400

    super

--- FocusSamples.cs
+++ FocusSamples.cs
@@ -14,7 +14,7 @@ public partial class FocusSamples : Control
 
     private void OnFocusEntered()
     {
-        GD.Print("Focus Entered!!!");
+        GD.Print("Focus Entered!!!!!!!");
         //focusedButton?.GrabFocus();
         label?.Text = focusedButton?.HasFocus().ToString();
     }
